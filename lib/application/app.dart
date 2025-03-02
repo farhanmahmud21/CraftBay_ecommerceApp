29ba7ae020b6297e447c373f736e3f84231160e0
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2025-03-02 23:07:21 +0600

    opening structure implemented

---
+++ lib/application/app.dart
@@ -0,0 +1,13 @@
+import 'package:e_commerce/presentation/ui/screens/splashSceen.dart';
+import 'package:flutter/material.dart';
+
+class CraftBay extends StatelessWidget {
+  const CraftBay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Splashsceen(),
+    );
+  }
+}
---
+++ lib/presentation/ui/screens/splashSceen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splashsceen extends StatelessWidget {
+  const Splashsceen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
