6d0d276d07feb6efbd4c9d2ba4a444d0360d46f5
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2025-03-03 00:02:30 +0600

    implement home screen and splash screen with navigation

--- lib/presentation/ui/screens/splashSceen.dart
+++ lib/presentation/ui/screens/splashSceen.dart
@@ -1,10 +1,57 @@
+import 'package:e_commerce/presentation/ui/screens/homeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class Splashsceen extends StatelessWidget {
+class Splashsceen extends StatefulWidget {
   const Splashsceen({super.key});
 
   @override
+  State<Splashsceen> createState() => _SplashsceenState();
+}
+
+class _SplashsceenState extends State<Splashsceen> {
+  void initState() {
+    super.initState();
+    goToHomeScreen();
+  }
+
+  void goToHomeScreen() {
+    Future.delayed(Duration(seconds: 2)).then((_) async {
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return HomeScreen();
+      }));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Spacer(),
+          Center(
+            child: SvgPicture.asset(
+              'assets/images/logo.svg',
+            ),
+          ),
+          Spacer(),
+          CircularProgressIndicator(
+            color: Color(0xFF27B8B9),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Version 0.1',
+            style: TextStyle(color: Color(0xFFA5A5A5)),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
   }
 }
